8fe5ab489c5b0f53636a1fcfc3e3cbd74c63aa6e
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2025-03-11 13:30:03 +0100

    ioReverse

--- main.hs
+++ main.hs
@@ -12,13 +12,35 @@ main2 = putStrLn "Hello World!" >>
        getLine >>= \input ->
        putStrLn input
 
+-- >> is used to sequence actions where the result of the first action is not needed.
+-- >>= is used to bind the result of getLine to the input variable.
+
 main3 :: IO ()
 main3 = do
       putStrLn "Hello"
 
--- >> is used to sequence actions where the result of the first action is not needed.
--- >>= is used to bind the result of getLine to the input variable.
 
+-- Consider the following simple IO function:
+-- ioReverse :: IO ()
+-- ioReverse = do
+-- putStrLn "Give me a string!"
+-- str <- getLine
+-- if null str then
+-- do
+-- putStrLn "Next time enter a non-empty string!"
+-- putStrLn "Capiche?!"
+-- else
+-- putStrLn ("The reverse of " ++ str ++ " is " ++ (reverse str) ++ ".")
+-- Transform this function to an equivalent function ioReverse' :: IO () which does the very
+-- same thing, but instead of the do-notation and <- use the binder function >>= and anonymous
+-- functions.
 
-
-
+main4 :: IO ()
+main4 =
+  putStrLn "Give me a string!" >>
+  getLine >>= \str ->
+  if null str then
+    putStrLn "Next time enter a non-empty string!" >>
+    putStrLn "Capiche?!"
+  else
+    putStrLn ("The reverse of " ++ str ++ " is " ++ reverse str ++ ".")
